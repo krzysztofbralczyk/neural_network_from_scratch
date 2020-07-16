45ef20b1c3020c61c7a25458c79713278ac8f754
Marek Kowalski <dylluan5121@gmail.com>
2020-07-16 18:54:48 +0200

    first try failed, no point in making neurons separate objects

--- first.py
+++ first.py
@@ -1,13 +1,15 @@
 import numpy as np
 
 
-class Neuron:
-    def __init__(self, name):
-        self.name = name;
+class Network:
+    def __init__(self, input_layer_size, output_layer_size, hidden_layers):
+        self.input_layer_size = input_layer_size
+        self.output_layer_size = output_layer_size
+        self.hidden_layers = hidden_layers
 
 
-# biases = [1, 2, 3, 4, 5]
-# weights = [0.2, 0.3, 0.4, 0.6, 0.7]
-
-neuron = Neuron("Krzysztof")
-print(neuron.name)
+weights = [[0.2, 0.3, 0.4, 0.6, 0.7], [0.5, 0.57, 0.46, 0.51, 0.37]]
+inputs = [1, 2, 3, 4, 5]
+bias = 1
+output = np.dot(weights, inputs) + bias
+print(output)
