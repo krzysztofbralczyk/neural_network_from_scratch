4bdf83d4d13198e9e6532e72df1a5b847a1cc0ce
Marek Kowalski <dylluan5121@gmail.com>
2020-07-16 16:24:17 +0200

    init

---
+++ first.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+class Neuron:
+    def __init__(self, name):
+        self.name = name;
+
+
+# biases = [1, 2, 3, 4, 5]
+# weights = [0.2, 0.3, 0.4, 0.6, 0.7]
+
+neuron = Neuron("Krzysztof")
+print(neuron.name)
