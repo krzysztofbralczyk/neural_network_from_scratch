77847ff76dbe4b7c705cd8fddbdfa6d0c9a1efa0
Krzysztof Bralczyk <krzysztofbralczyk@gmail.com>
2020-07-17 23:41:37 +0200

    Layer object is probably also useless right now, let's try with only network object

--- first.py
+++ first.py
@@ -1,25 +1,75 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2
+# import matplotlib.pyplot as plt
+# from PIL import Image
 
 
 class Layer:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.15 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
+        self.weights = 0.15 * np.random.randn(n_neurons, n_inputs)
+        self.biases = np.zeros(n_neurons)
         self.output = None
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-
-X = [[1, 2, 3, 4, 5],
-     [5, 2, 7, 1, 7],
-     [4, 7, 33, 5, 7],
-     [2, 5, 7, 4, 8]]
-# weights1 = [[0.2, 0.3, 0.4, 0.6, 0.7], [0.5, 0.57, 0.46, 0.51, 0.37], [0.9, 0.2, 0.5, 0.6, 0.1]]
-# biases1 = [1, 2, 3]
+    def test(self):
+        print("layer object is working")
+        print(self.weights)
+        print(self.biases)
+        print()
 
 
-layer_one = Layer(5, 1)
-layer_one.forward(X)
-print(layer_one.output)
+class Network:
+    def __init__(self, layers_size, X, y, iterations, learning_rate):
+        self.X = X
+        self.y = y
+        self.layers_size = layers_size
+        self.layers = []
+
+    def train(self):
+        for i in self.iterations:
+            self.feedforward()
+            self.calculate_cost()
+            self.calculate_gradient()
+            self.backpropagation()
+
+    def create_structure(self):
+        for idx, num in enumerate(self.layers_size[1:]):
+            # print(num, end=' ')
+            # print(idx)
+            self.layers.append(Layer(self.layers_size[idx], num))
+
+    def test_structure(self):
+        for layer in self.layers:
+            layer.test()
+
+    def activation_function(self, number):
+        return 1/(1+np.exp(number))
+
+
+# images = []
+# results = []
+# for i in range(10):
+#     images.append(cv2.resize(cv2.imread("./assets/cat.jpg"), (250, 250)))
+#     images.append(cv2.resize(cv2.imread("./assets/dog.jpg"), (250, 250)))
+# images = np.array(images)
+# # print(images)
+# # print(images.shape)
+# images_flattened = images.reshape(images.shape[0], -1).T
+# images_flattened = images_flattened / 255
+# print(images_flattened.shape)
+
+network = Network([9, 5, 3, 2])
+network.create_structure()
+# network.test_structure()
+
+
+# X = [[1, 2, 3, 4, 5],
+#      [5, 2, 7, 1, 7],
+#      [4, 7, 33, 5, 7],
+#      [2, 5, 7, 4, 8]]
+#
+# layer_one = Layer(5, 1)
+# layer_one.forward(X)
+# print(layer_one.output)
--- pillow_testing.py
+++ pillow_testing.py
@@ -1,6 +1,24 @@
 from __future__ import print_function
 from PIL import Image
+import numpy as np
 
 im = Image.open("./assets/cat.jpg")
-print(im.format, im.size, im.mode)
-im.show()
+im_arr = np.array(im)
+print(type(im_arr))
+print(im_arr)
+
+im2 = Image.fromarray(im_arr)
+im2.show()
+# print(im.format, im.size, im.mode)
+# im.show()
+# im.thumbnail((500, 500))
+# r, g, b = im.split()
+# print(r)
+# r.show()
+# g.show()
+# b.show()
+# merged_im = Image.merge("RGB", (r, g, b))
+
+# merged_im.show()
+# pixels = list(r.getdata())
+# print(pixels)
