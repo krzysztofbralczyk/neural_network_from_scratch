9b6efb00e88c0bcfd14061d195bb98d5f3146d29
Krzysztof Bralczyk <krzysztofbralczyk@gmail.com>
2020-07-19 10:24:48 +0200

    changed overall form

--- first.py
+++ first.py
@@ -1,51 +1,78 @@
 import numpy as np
+import time
+import random
 import cv2
+
+
 # import matplotlib.pyplot as plt
 # from PIL import Image
 
 
-class Layer:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.15 * np.random.randn(n_neurons, n_inputs)
-        self.biases = np.zeros(n_neurons)
-        self.output = None
+def sigmoid(z):
+    """Activation function"""
+    return 1.0 / (1.0 + np.exp(-z))
 
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
 
-    def test(self):
-        print("layer object is working")
-        print(self.weights)
-        print(self.biases)
-        print()
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z) * (1 - sigmoid(z))
 
 
 class Network:
-    def __init__(self, layers_size, X, y, iterations, learning_rate):
-        self.X = X
-        self.y = y
-        self.layers_size = layers_size
-        self.layers = []
+    def __init__(self, layers_sizes_array):
+        print("---------------\nInitialized network object")
+        # time.sleep(1)
+        self.num_layers = len(layers_sizes_array)
+        print("Set num_layers to: {0}".format(self.num_layers))
+        # time.sleep(1)
+        self.layers = layers_sizes_array
+        print("Set layers to: {0}".format(self.layers))
+        # time.sleep(1)
+        self.biases = [np.random.randn(y, 1) for y in self.layers[1:]]
+        print("\nSet biases to a list of numpy arrays with length: {0}".format(len(self.biases)))
+        # time.sleep(1)
+        for i, bias in enumerate(self.biases):
+            print("{0}st array in list of biases is vector with shape(rows,columns): {1}".format(i, bias.shape))
+            # time.sleep(1)
 
-    def train(self):
-        for i in self.iterations:
-            self.feedforward()
-            self.calculate_cost()
-            self.calculate_gradient()
-            self.backpropagation()
+        self.weights = [np.random.randn(y, x) for x, y in zip(self.layers[:-1], self.layers[1:])]
+        print("\nSet weights to a list of numpy 2D arrays with length: {0}".format(len(self.weights)))
+        # time.sleep(1)
+        for i, weight in enumerate(self.weights):
+            print("{0}st array in list of weights is matrix with shape(rows,columns): {1}, first number symbolising "
+                  "number of neurons, and second symbolising number of inputs to each neuron".format(i, weight.shape))
+            # time.sleep(1)
+        print("---------------")
 
-    def create_structure(self):
-        for idx, num in enumerate(self.layers_size[1:]):
-            # print(num, end=' ')
-            # print(idx)
-            self.layers.append(Layer(self.layers_size[idx], num))
+    def feedforward(self, a):
+        """Return the output of the network if 'a' is an input"""
+        print("---------------\n Feedforward activated")
+        for i, (b, w) in enumerate(zip(self.biases, self.weights)):
+            # print("{0}st weights matrix 'w' is \n{1}".format(i, w))
+            # time.sleep(2)
+            # print("{0}st input vector 'a' is\n {1}".format(i, a))
+            # time.sleep(2)
+            # print("{0}st biases vector 'b' is\n {1}".format(i, b))
+            # time.sleep(2)
+            a = sigmoid(np.dot(w, a) + b)
+            # print("{0}st output and also next layer input is\n {1}".format(i, a))
+            # time.sleep(2)
+        print("---------------")
+        return a
 
-    def test_structure(self):
-        for layer in self.layers:
-            layer.test()
 
-    def activation_function(self, number):
-        return 1/(1+np.exp(number))
+network = Network([5, 4, 3, 2])
+network.feedforward(np.random.randn(5, 1))
+
+
+
+
+
+
+
+
+
+
 
 
 # images = []
@@ -60,8 +87,8 @@ class Network:
 # images_flattened = images_flattened / 255
 # print(images_flattened.shape)
 
-network = Network([9, 5, 3, 2])
-network.create_structure()
+# network = Network([9, 5, 3, 2])
+# network.create_structure()
 # network.test_structure()
 
 
